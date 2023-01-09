155f88048a9a7515b50f6434650a03b737e663d8
sarwar-asik <shasik2021@gmail.com>
2023-01-09 17:04:59 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import "flowbite";
+import UserForm from "./components/UserForm";
+
+function App() {
+  return (
+    <div className="bg-slate-400 py-10">
+      <UserForm/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/UserForm.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const UserForm = () => {
+    return (
+        <div>   
+        </div>
+    );
+};
+
+export default UserForm;
